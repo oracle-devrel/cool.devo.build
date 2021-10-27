ce8608cfcf8833712d735030826123590fe4148e
Brett Terpstra <me@brettterpstra.com>
2021-10-27 15:46:05 -0500

    Initial OHANA commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,56 @@
+if (!window.cool) {
+    var cool = {};
+}
+
+cool.slides = (function() {
+    function goToSlide(x) {
+      $('html, body').animate({
+        scrollTop: $("#slideshow").offset().top
+      }, 300, function() {
+        $('.slide.active').fadeOut(300, function(e) {
+          $('.active').removeClass('active');
+          $('*[data-target-slide=' + x + ']').addClass('active');
+          $('#slide-' + x).fadeIn(300).addClass('active');
+        });
+      });
+
+    }
+
+    return {
+        goToSlide: goToSlide
+    };
+
+}());
+
+
+cool.ohana = (function() {
+  function bind() {
+    const paths = ['diy', 'mix', 'managed'];
+
+    $(paths.map(p => `.${p}`).join(', ')).fadeOut();
+
+    $('.pathnav a.path').on('click', function(e) {
+      $('body').addClass('active');
+      $this = $(this);
+      $('.pathnav .active').removeClass('active');
+      $this.addClass('active');
+
+      let path = $this.attr('data-path');
+
+      $(paths.filter(p => p !== path).map(p => `.${p}`).join(', '))
+        .not(`.${path}`)
+        .fadeOut('fast', function() {
+          $(`.${path}`).fadeIn();
+        });
+
+    });
+  };
+
+  return {
+    bind: bind
+  };
+}());
+
 $(document).ready(function() {
   $('.classifications__nav a').on('click', function(e) {
     $('.classifications section.active, .tagposts__tag.active').removeClass('active');
@@ -28,28 +81,9 @@ $(document).ready(function() {
     moreLink: '<a href="#">More&hellip;</a>',
     lessLink: '<a href="#">Less&hellip;</a>',
     embedCSS: true });
+
+  if ($('body').attr('id') === 'ohana') {
+    cool.ohana.bind();
+  }
 });
 
-if (!window.cool) {
-    var cool = {};
-}
-
-cool.slides = (function() {
-    function goToSlide(x) {
-      $('html, body').animate({
-        scrollTop: $("#slideshow").offset().top
-      }, 300, function() {
-        $('.slide.active').fadeOut(300, function(e) {
-          $('.active').removeClass('active');
-          $('*[data-target-slide=' + x + ']').addClass('active');
-          $('#slide-' + x).fadeIn(300).addClass('active');
-        });
-      });
-
-    }
-
-    return {
-        goToSlide: goToSlide
-    };
-
-}());
